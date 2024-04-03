1025b4f0d3cf40e2bfed1f494e5d7b3d64426644
hossein <hosseinbakhtiari781@outlook.com>
2024-04-03 20:32:53 +0330

    add styles to Table Coins

--- src/components/modules/TableCoins.jsx
+++ src/components/modules/TableCoins.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import chartUp from "../../assets/chart-up.svg"
 import chartDown from "../../assets/chart-down.svg"
 import { RotatingLines } from 'react-loader-spinner';
+import styles from "./TableCoins.module.css"
 
 function TableCoins({coins , isLoading}) {
     console.log(coins);
   return (
-    <div>
+    <div className={styles.container}>
         {
           isLoading ? (
               <RotatingLines strokeColor='#3874ff' strokeWidth='2' /> 
           ) : (
-            <table>
+            <table className={styles.table}>
             <thead>
               <tr>
                 <th>Coin</th>
@@ -52,14 +53,14 @@ const TableRow = ({
     return(
       <tr>
       <td>
-        <div>
+        <div className={styles.symbol}>
           <img src={image} />
           <span>{symbol.toUpperCase()}</span>
         </div>
        </td>
        <td>{name}</td>
        <td>${current_price.toLocaleString()}</td>
-       <td>{price_cheange.toFixed(2)}%</td>
+       <td className={price_cheange > 0 ? styles.success : styles.error}>{price_cheange.toFixed(2)}%</td>
        <td>{total_volume.toLocaleString()}</td>
        <td><img src={price_cheange > 0 ? chartUp : chartDown} /></td>
     </tr>
