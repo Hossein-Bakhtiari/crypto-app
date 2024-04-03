06fbd96fb4dc8a245af7b4e4c1b6fccbd5ff581e
hossein <hosseinbakhtiari781@outlook.com>
2024-04-03 17:52:23 +0330

    first commit

---
+++ src/components/modules/TableCoins.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TableCoins({coins}) {
+    console.log(coins);
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default TableCoins
---
+++ src/components/templates/HomePage.jsx
@@ -0,0 +1,26 @@
+import React, { useState ,useEffect } from 'react'
+
+import TableCoins from '../modules/TableCoins';
+import { getCoinList } from '../services/cryptoApi';
+
+function HomePage() {
+    const [coins , setCoins] = useState([]);
+    useEffect(() => {
+        const getData = async () => {
+            const res = await fetch(getCoinList());
+            const json = await res.json();
+            setCoins(json);
+        }
+
+        getData();
+    } , [])
+  
+    return (
+        <div>
+            <TableCoins coins={coins}/>
+        </div>
+  
+    )
+}
+
+export default HomePage
