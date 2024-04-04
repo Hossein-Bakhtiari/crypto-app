abf305668b950f7754da544006c482e8664ebdf4
hossein <hosseinbakhtiari781@outlook.com>
2024-04-04 14:51:33 +0330

    lifting state Up from pagination component to the HomePage components

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react'
 
-function Pagination() {
-    const [page , setPage] = useState(1)
-    
-
+function Pagination({page , setPage}) { 
     const priviousHandler = () => {
         if(page <= 1 ) return;
         setPage(((page) => page - 1))
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -7,6 +7,8 @@ import Pagination from '../modules/Pagination';
 function HomePage() {
     const [coins , setCoins] = useState([]);
     const [isLoading , setIsLoading] = useState(true);
+    const [page , setPage] = useState(1)
+
     useEffect(() => {
         const getData = async () => {
             const res = await fetch(getCoinList());
@@ -20,7 +22,7 @@ function HomePage() {
   
     return (
         <div>
-            <Pagination />
+            <Pagination page={page} setPage={setPage}/>
             <TableCoins coins={coins} isLoading={isLoading}/>
         </div>
   
