41c060553cb9d98be5200676fe75ed3e305a06b3
hossein <hosseinbakhtiari781@outlook.com>
2024-04-05 23:14:55 +0330

    convert the data

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./chart.module.css"
+import { ConvertData } from '../helpers/ContvertData';
 
 function Chart({chart , setChart}) {
+
+    const [type, setType] = useState("prices")
+    console.log(ConvertData( chart , type));
   return (
     <div className={styles.container}>
         <span className={styles.cross} onClick={() => setChart(null)}>X</span>
