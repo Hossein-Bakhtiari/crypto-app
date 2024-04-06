3c8f066316c5a5310f3be067d79272ecac505eae
hossein <hosseinbakhtiari781@outlook.com>
2024-04-06 09:28:48 +0330

    draw the charts with Charts library

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styles from "./chart.module.css"
 import { ConvertData } from '../helpers/ContvertData';
+import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, YAxis} from "recharts";
 
 function Chart({chart , setChart}) {
 
@@ -10,10 +11,36 @@ function Chart({chart , setChart}) {
     <div className={styles.container}>
         <span className={styles.cross} onClick={() => setChart(null)}>X</span>
         <div className={styles.chart}>
-
+            <div className={styles.graph}>
+                <ChartComponent data={ConvertData( chart , type)} type={type}/>
+            </div>
         </div>
     </div>
   )
 }
 
 export default Chart
+
+
+const ChartComponent = ({data , type}) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+    <LineChart 
+      width={400} 
+      height={400}
+      data={data}>
+          <Line 
+            type="monotone" 
+            dataKey={type} 
+            stroke="#3874ff" 
+            strokeWidth="2px"/>
+  
+          <CartesianGrid stroke='#404042' />
+          <YAxis dataKey={type} domain={["auto" , "auto"]}/>
+          <YAxis dataKey="date" hide />
+          <Legend  />
+          <Tooltip />
+    </LineChart>
+  </ResponsiveContainer>
+  )
+}
